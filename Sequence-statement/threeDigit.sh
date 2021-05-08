3ef535aa6e1fc5f7d4cb25433db34c6d01422aae
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-05-08 19:43:17 +0530

    New files are added

---
+++ Sequence-statement/threeDigit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+
+num1=$(((RANDOM%900)+100))
+num2=$(((RANDOM%900)+100))
+num3=$(((RANDOM%900)+100))
+num4=$(((RANDOM%900)+100))
+num5=$(((RANDOM%900)+100))
+
+sum=($num1+$num2+$num3+$num4+$num5)
+
+echo $sum
---
+++ Sequence-statement/weekProgramme.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+echo "enter a number here" num
+read num
+
+   if [ $num==1 ]
+
+     then
+     echo "sunday"
+
+     elif [ $num==2 ]
+
+      then
+     echo "monday"
+
+     elif  [ $num==3 ]
+
+      then
+     echo "tuesday"
+
+      elif  [ $num==4 ]
+
+      then
+     echo "wednesday"
+
+     elif  [ $num==3 ]
+
+      then
+     echo "thurseday"
+
+     elif  [ $num==3 ]
+
+      then
+     echo "friday"
+
+     elif  [ $num==3 ]
+
+      then
+     echo "saturday"
+fi
